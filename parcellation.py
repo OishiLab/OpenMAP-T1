284106ee1afef2fc1f6594a5008adb300fb0861b
ke-lab1106 <kei.nishimaki.1106@gmail.com>
2024-02-28 14:15:05 -0500

    20240228

--- parcellation.py
+++ parcellation.py
@@ -34,7 +34,7 @@ def main():
     print(
         "\n#######################################################################\n"
         "Please cite the following paper when using OpenMAP-T1:\n"
-        "Kei Nishimaki, Kengo Onda, Kumpei Ikuta, Yuto Uchida, Hitoshi Iyatomi, Kenichi Oishi (2024).\n"
+        "Kei Nishimaki, Kengo Onda, Kumpei Ikuta, Jill Chotiyanonta, Yuto Uchida, Hitoshi Iyatomi, Kenichi Oishi (2024).\n"
         "OpenMAP-T1: A Rapid Deep Learning Approach to Parcellate 280 Anatomical Regions to Cover the Whole Brain.\n"
         "paper: https://www.medrxiv.org/content/10.1101/2024.01.18.24301494v1.\n"
         "#######################################################################\n"
