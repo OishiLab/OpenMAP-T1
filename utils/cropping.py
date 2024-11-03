1ecb036812240b5df2d71b95eef02f23018374e7
西牧慧 <macky@mackyMacBook-Air.local>
2024-11-03 14:08:58 +0900

    20241103

--- utils/cropping.py
+++ utils/cropping.py
@@ -6,6 +6,17 @@ from utils.functions import normalize
 
 
 def crop(voxel, model, device):
+    """
+    Crops the given voxel data using the provided model and device.
+
+    Args:
+        voxel (numpy.ndarray): The input voxel data to be cropped, expected to be of shape (N, 256, 256).
+        model (torch.nn.Module): The PyTorch model used for cropping.
+        device (torch.device): The device (CPU or GPU) on which the computation will be performed.
+
+    Returns:
+        torch.Tensor: The cropped output tensor of shape (256, 256, 256).
+    """
     model.eval()
     with torch.inference_mode():
         output = torch.zeros(256, 256, 256).to(device)
@@ -13,20 +24,40 @@ def crop(voxel, model, device):
             image = v.reshape(1, 1, 256, 256)
             image = torch.tensor(image).to(device)
             x_out = torch.sigmoid(model(image)).detach()
-            if i == 0:
-                output[0] = x_out
-            else:
-                output[i] = x_out
+            output[i] = x_out
         return output.reshape(256, 256, 256)
 
 
 def closing(voxel):
+    """
+    Perform a binary closing operation on a 3D voxel array.
+
+    This function applies a binary closing operation using a 3x3x3 structuring element
+    and performs the operation for a specified number of iterations.
+
+    Parameters:
+    voxel (numpy.ndarray): A 3D numpy array representing the voxel data to be processed.
+
+    Returns:
+    numpy.ndarray: The voxel data after the binary closing operation.
+    """
     selem = np.ones((3, 3, 3), dtype="bool")
     voxel = binary_closing(voxel, structure=selem, iterations=3)
     return voxel
 
 
 def cropping(data, cnet, device):
+    """
+    Crops the input medical imaging data using a neural network model.
+
+    Args:
+        data (nibabel.Nifti1Image): The input medical imaging data in NIfTI format.
+        cnet (torch.nn.Module): The neural network model used for cropping.
+        device (torch.device): The device (CPU or GPU) on which the model is run.
+
+    Returns:
+        numpy.ndarray: The cropped medical imaging data.
+    """
     voxel = data.get_fdata()
     voxel = normalize(voxel)
 
